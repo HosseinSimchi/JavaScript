85a0ef61d719bf8ffd0d621c06de7ebcdf439a5b
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-05-26 13:06:34 +0430

    Third practice

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-var Hossein = function () {
-    alert("Hello Hossein")
+// Set the date we're counting down to
+var countDownDate = new Date().getTime();
+countDownDate -= 120;
+
+console.log(countDownDate);
+let x;
+function stopping() {
+    clearInterval(window.x);
+    document.getElementById("start").disabled  = false;
+    document.getElementById("stop").disabled  = true;
+}
+
+function starting() {
+    document.getElementById("start").disabled  = true;
+    document.getElementById("stop").disabled  = false;
+    window.x = setInterval (function starting() {
+        var now = new Date().getTime();
+        var distance = now - countDownDate;
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          
+        // Output the result in an element with id="demo"
+        document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+          
+        // If the count down is over, write some text 
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+},1000)
+
 }

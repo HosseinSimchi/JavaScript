fe174f2e66e429e0100e7e16c1fd661fc87f1871
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-05-26 21:59:35 +0430

    Commited

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 // Set the date we're counting down to
 
 let x;
-var countDownDate1 = new Date("Jan 5, 2024 15:37:25").getTime();
-var countDownDate2 = new Date("Jan 5, 2024 15:39:25").getTime();
-let distance = countDownDate2 - countDownDate1;
+let countDownDate1 = new Date("Jan 5, 2024 15:37:25").getTime();
+let countDownDate2 = new Date("Jan 5, 2024 15:39:25").getTime();
+let distance = countDownDate2 - countDownDate1; 
+
 function stopping() {
     clearInterval(window.x);
     document.getElementById("start").disabled  = false;
     document.getElementById("stop").disabled  = true;
 }
 
+function resetting() {
+  clearInterval(window.x);
+  document.getElementById("start").disabled  = false;
+  document.getElementById("stop").disabled  = true;
+  document.getElementById("stop").disabled  = true;
+  document.getElementById("demo").innerHTML = "00" + "m " + "00" + "s ";
+  distance = countDownDate2 - countDownDate1;
+}
+
+
 function starting() {
     document.getElementById("start").disabled  = true;
+    document.getElementById("stop").disabled  = true;
     document.getElementById("stop").disabled  = false;
-        window.x = setInterval (function starting() {
-          distance--;
+        window.x = setInterval (function settinginterval() {
+          distance -= 1000
           var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
           var seconds = Math.floor((distance % (1000 * 60)) / 1000);
           document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";

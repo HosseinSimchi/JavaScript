f4798cd9223d4ca566163d2eda17be3e95c09fe7
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-05-26 21:34:30 +0430

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Set the date we're counting down to
-var countDownDate = new Date().getTime();
-countDownDate -= 120;
 
-console.log(countDownDate);
 let x;
+var countDownDate1 = new Date("Jan 5, 2024 15:37:25").getTime();
+var countDownDate2 = new Date("Jan 5, 2024 15:39:25").getTime();
+let distance = countDownDate2 - countDownDate1;
 function stopping() {
     clearInterval(window.x);
     document.getElementById("start").disabled  = false;
@@ -13,20 +13,14 @@ function stopping() {
 function starting() {
     document.getElementById("start").disabled  = true;
     document.getElementById("stop").disabled  = false;
-    window.x = setInterval (function starting() {
-        var now = new Date().getTime();
-        var distance = now - countDownDate;
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-          
-        // Output the result in an element with id="demo"
-        document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
-          
-        // If the count down is over, write some text 
-        if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("demo").innerHTML = "EXPIRED";
-        }
-},1000)
-
+        window.x = setInterval (function starting() {
+          distance--;
+          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+          if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+          }
+        },1000)
 }
